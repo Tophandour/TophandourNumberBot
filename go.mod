16ed86b3512c51a57c98fcced19cc320cbfd1e25
Tophandour <Tophandour@gmail.com>
2021-05-25 01:30:39 -0400

    Creating most of bot
    Most of bot built, needs cleaning up

---
+++ go.mod
@@ -0,0 +1,8 @@
+module TophandourNumberBot
+
+go 1.16
+
+require (
+	github.com/bwmarrin/discordgo v0.23.2 // indirect
+	github.com/dghubble/go-twitter v0.0.0-20201011215211-4b180d0cc78d // indirect
+)
--- main.go
+++ main.go
@@ -1,10 +1,54 @@
-// main
+//main
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"regexp"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/dghubble/go-twitter/twitter"
 )
 
-func main() {
+var phoneNumberRegex = regexp.MustCompile(`(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}`)
 
+func getTweets(bearerString string) []twitter.Tweet {
+	urlQuery := "https://api.twitter.com/1.1/search/tweets.json?q=" + url.QueryEscape(`("my number is") OR ("call me") OR ("phone number") OR ("call")`) + "&result_type=recent&tweet_mode=extended&count=100&lang=en"
+
+	client := &http.Client{}
+
+	req, _ := http.NewRequest("GET", urlQuery, nil)
+
+	req.Header.Add("Authorization", "Bearer "+bearerString)
+
+	resp, _ := client.Do(req)
+	defer resp.Body.Close()
+	bodybytes, _ := ioutil.ReadAll(resp.Body)
+	bodyAsString := string(bodybytes)
+	responseObject := twitter.Search{}
+	json.Unmarshal([]byte(bodyAsString), &responseObject)
+	return responseObject.Statuses
+}
+
+func postDiscord(message string, botString string, channelString string) {
+	discord, _ := discordgo.New("Bot " + botString)
+	discord.Open()
+	discord.ChannelMessageSend(channelString, message)
+	discord.Close()
+}
+
+func main() {
+	tweetBearerString := string(os.Args[1])
+	botSecretString := string(os.Args[2])
+	channelIDString := string(os.Args[3])
+
+	tweetList := getTweets(tweetBearerString)
+	for i := range tweetList {
+		if phoneNumberRegex.MatchString(tweetList[i].FullText) {
+			postDiscord(">>> https://twitter.com/i/web/status/"+tweetList[i].IDStr, botSecretString, channelIDString)
+		}
+	}
 }
